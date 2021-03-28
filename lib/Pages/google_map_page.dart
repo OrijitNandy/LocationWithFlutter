09802ae9cbdae91eeb2b01d7df92eeaf23a51b20
Orijit <norijit@gmail.com>
2021-03-28 07:51:50 -0400

    Location Marking v0.00003(3 marker,info view,change in codebase)

--- lib/Pages/google_map_page.dart
+++ lib/Pages/google_map_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -7,41 +8,36 @@ class GoogleMapPage extends StatefulWidget {
 }
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
-  Map<MarkerId, Marker> markers = {};
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
   List listMarkerIds = List.filled(500, 0, growable: true);
   static final LatLng _center = const LatLng(22.342088, 91.836738);
 
   void _onMapCreated(GoogleMapController controller) {
-    MarkerId markerId1 = MarkerId("1");
-    MarkerId markerId2 = MarkerId("2");
-    MarkerId markerId3 = MarkerId("3");
+    int generateIds() {
+      var rng = new Random();
+      var randomInt;
+      randomInt = rng.nextInt(100);
+      print(rng.nextInt(100));
+      return randomInt;
+    }
 
-    listMarkerIds.add(markerId1);
-    listMarkerIds.add(markerId2);
-    listMarkerIds.add(markerId3);
-    Marker marker1 = Marker(
-        markerId: markerId1,
-        position: LatLng(22.348002, 91.833913),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-        infoWindow: InfoWindow(
-            title: "Jamal Khan Circle", snippet: "22.348002, 91.833913"));
-    Marker marker2 = Marker(
-      markerId: markerId2,
-      position: LatLng(22.342162, 91.830539),
-      infoWindow:
-          InfoWindow(title: "DC Hill Circle", snippet: "22.342162, 91.830539"),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-    );
-    Marker marker3 = Marker(
-        markerId: markerId3,
-        position: LatLng(22.342117, 91.836728),
-        infoWindow: InfoWindow(
-            title: "Anderkilla Circle", snippet: "22.342117, 91.836728"));
-    setState(() {
-      markers[markerId1] = marker1;
-      markers[markerId2] = marker2;
-      markers[markerId3] = marker3;
-    });
+    Future<void> buildMarker() async {
+      var gridItem;
+      for (var i = 0; i < gridItem.location.length; i++) {
+        var markerIdVal = generateIds();
+        final MarkerId markerId = MarkerId(markerIdVal.toString());
+        final Marker marker = Marker(
+            markerId: markerId,
+            position: LatLng(gridItem.locations[i].latitude,
+                gridItem.locations[i].longitude),
+            infoWindow: InfoWindow(
+                title: gridItem.locations[i].place,
+                snippet: gridItem.locations[i].region));
+        setState(() {
+          markers[markerId] = marker;
+        });
+      }
+    }
   }
 
   @override
@@ -51,9 +47,9 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
           title: Text("Google Map"),
         ),
         body: GoogleMap(
-            onMapCreated: _onMapCreated,
-            markers: Set.of(markers.values),
-            initialCameraPosition:
-                CameraPosition(target: _center, zoom: 12.toDouble())));
+          onMapCreated: _onMapCreated,
+          markers: Set.of(markers.values),
+          initialCameraPosition: CameraPosition(target: _center, zoom: 12),
+        ));
   }
 }
